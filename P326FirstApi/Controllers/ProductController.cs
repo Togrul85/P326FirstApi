7ba1f2b31881cd6aeb36824cb2b20d64c5aa7c2e
Togrul85 <toghrulkhs@code.edu.az>
2023-04-11 14:10:34 +0400

    ListStartToEnd

---
+++ P326FirstApi/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P326FirstApi.Models;
+
+namespace P326FirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new()
+        {
+
+            new(){Id= 1, Name="Product1",SalePrice=570},
+             new(){Id= 2, Name="Product2",SalePrice=530},
+              new(){Id= 3, Name="Product3",SalePrice=520},
+               new(){Id= 4, Name="Product4",SalePrice=510},
+
+        };
+
+
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return StatusCode(200, products);
+
+            //return Ok(new {Code=1001,products});
+        }
+        [Route("{id}")]
+        [HttpGet]
+
+        public IActionResult GetOne(int id)
+        { 
+       Product product = products.FirstOrDefault(p => p.Id == id);
+        if (product == null) return StatusCode(StatusCodes.Status404NotFound);
+            return Ok(product); 
+        }
+
+        [HttpPost]
+
+        public IActionResult AddProduct(Product product) 
+        { 
+        products.Add(product);
+            return StatusCode(StatusCodes.Status201Created, product);
+        }
+
+    }
+}
+
+
---
+++ P326FirstApi/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace P326FirstApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double SalePrice { get; set; }
+
+        public double CostPrice { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
